bb859ecc5f60ad7a56937d6b1682c546b1f9a988
Steeph <Nephadwlp@gmail.com>
2015-10-18 18:31:19 +0200

    Symf Bundle

---
+++ KSC_symf/README.md
@@ -0,0 +1,4 @@
+symfonu_KSC
+===========
+
+A Symfony project created on October 18, 2015, 6:23 pm.
---
+++ KSC_symf/src/KS/KscBundle/KSKscBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace KS\KscBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KSKscBundle extends Bundle
+{
+}
