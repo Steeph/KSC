25598cc9666d3bad9bb51839af1a8029412a85ed
Steeph <Nephadwlp@gmail.com>
2015-10-18 18:36:56 +0200

    Suppression

--- KSC_symf/README.md
+++
@@ -1,4 +0,0 @@
-symfonu_KSC
-===========
-
-A Symfony project created on October 18, 2015, 6:23 pm.
--- KSC_symf/src/KS/KscBundle/KSKscBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace KS\KscBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class KSKscBundle extends Bundle
-{
-}
